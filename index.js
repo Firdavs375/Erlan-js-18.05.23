eaeaab0e6653c70dc2679ee9d9c97cd69cee486b
Firdavs375 <firdavss.3755@gmail.com>
2023-05-18 19:04:45 +0500

    flex

---
+++ index.js
@@ -0,0 +1,15 @@
+// [1.Convert Minutes into Seconds](https://edabit.com/challenge/8q54MKnRrm89pSLmW)
+
+function convert(minutes) {
+	return minutes * 60
+}
+//
+
+
+// [2.Return the Sum of Two Numbers](https://edabit.com/challenge/3LpBLgNRyaHMvNb4j)
+
+function addition(a, b) {
+	return a + b
+}
+
+//
